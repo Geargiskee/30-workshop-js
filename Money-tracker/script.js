8d924fba45917d57b50ecadef4ea1f060081533c
Geargiskee <104674986+Geargiskee@users.noreply.github.com>
2022-07-04 21:21:59 +0700

    money-tracker

--- Money-tracker/script.js
+++ Money-tracker/script.js
@@ -1,3 +1,5 @@
+ 
+
 const balance = document.getElementById('balance')
 const money_plus = document.getElementById('money-plus')
 const money_minus = document.getElementById('money-minus')
@@ -7,11 +9,63 @@ const text = document.getElementById('text')
 const amount = document.getElementById('amount')
 
 const dataTransaction = [
-    {id:1, text:"ค่าขนม", amount:-400},
-    {id:2, text:"ค่าห้อง", amount:-5000},
-    {id:3, text:"เงินเดือน", amount:20000}
-
 ]
+function autoId(){
+    return Math.floor(Math.random()*1000000)
+}
+function init(){
+    list.innerHTML = ''
+    transaction.forEach(addDataToList)
+    calculateMoney()
+}
+let transaction = dataTransaction;
+function addDataToList(transaction){
+    const symbol = transaction.amount < 0 ?'-':'+';
+    const status = transaction.amount < 0 ?'minus':'plus'
+    const item = document.createElement('li')
+    result = formatNumber(Math.abs(transaction.amount))
+    item.classList.add(status)
+    // item.innerHTML = 'ค่าซ่อมรถ <span> -400 </span> <button class="delete-btn"></button>'
+    item.innerHTML = `${transaction.text}<span>${symbol}${result}</span><button class="delete-btn" onclick="removeData(${transaction.id})">x</button>`
+    list.appendChild(item)
 
-const transaction = dataTransaction;
-console.log(transaction)
+}
+function calculateMoney(){
+    const amounts = transaction.map(transaction=>transaction.amount)
+    const total = amounts.reduce((result, item)=>(result += item), 0).toFixed(2)
+    const income =amounts.filter(item=>item>0).reduce((result, item)=>(result += item), 0).toFixed(2)
+    const outcome = (amounts.filter(item=>item<0).reduce((result, item)=>(result += item), 0)*-1).toFixed(2)
+    
+    balance.innerText = formatNumber(total)
+    money_plus.innerText = formatNumber(income)
+    money_minus.innerText = formatNumber(outcome)
+}
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  }
+
+function removeData(id){
+    transaction = transaction.filter(transaction=>transaction.id !== id)
+    init()
+}
+function addTransaction(e){
+    e.preventDefault()
+    if(text.value.trim() === '' || amount.value.trim() === ''){
+        alert("เติมข้อมูลด้วย")
+    }else{
+            const data ={
+                id: autoId(),
+                text: text.value,
+                amount: +amount.value
+            }
+            transaction.push(data)
+            addDataToList(data)
+            calculateMoney()
+            text.value=''
+            amount.value=''
+    }
+}
+
+
+form.addEventListener('submit', addTransaction)
+init()
